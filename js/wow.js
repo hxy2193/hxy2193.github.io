05c20a494c4d99ba1d25f8ce76fe9f4d74f39235
Your Name <1056184686@qq.com>
2016-11-03 23:39:45 +0800

    'sd'

--- js/wow.js
+++ js/wow.js
@@ -1,4 +1,5 @@
 (function() {
+  
   var MutationObserver, Util, WeakMap,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
